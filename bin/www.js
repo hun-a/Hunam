096615318e4a38e2729495fa27a02d657113b839
seunghun <seunghun_kim@cubrid.com>
2017-08-10 18:30:21 +0900

    add static to www.js

--- bin/www.js
+++ bin/www.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 const app = express();
 const port = 10000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  fs.readFile(`${__dirname}/../api/index.html`,(err, data) => {
+  fs.readFile(`${__dirname}/../api/index.html`, "utf8", (err, data) => {
     if (err) {
       console.log(err);
       return;
