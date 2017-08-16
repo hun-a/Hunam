f96bd1477d2da63ecedd7a4187c1b18cc74e15f2
seunghun <seunghun.chan@gmail.com>
2017-08-17 00:47:53 +0900

    change html to pug

--- bin/www.js
+++ bin/www.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 10000;
 const Downloader = require('../utils/Downloader');
 
+app.set('view engine', 'pug');
+app.locals.pretty = true;
 app.use(express.static('public'));
 
 app.get('/down/:url/:name', (req, res) => {
@@ -22,13 +24,7 @@ app.get('/down/:url/:name', (req, res) => {
 });
 
 app.get('/play', (req, res) => {
-  fs.readFile(`${__dirname}/../api/index.html`, "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    res.send(data);
-  });
+  res.render('play');
 });
 
 app.listen(port, () => {
