fdbb7455720036a2c156c85b347aea74a0d93280
seunghun <seunghun_kim@cubrid.com>
2017-08-16 20:04:43 +0900

    add download url

--- bin/www.js
+++ bin/www.js
@@ -2,10 +2,26 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = 10000;
+const Downloader = require('../utils/Downloader');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/down/:url/:name', (req, res) => {
+  const url = req.params.url;
+  const name = req.params.name;
+
+  Downloader.download(url, name);
+
+  Downloader.on('complete', (err, data) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    res.send('<script>alert("Download Complete")</script>');
+  });
+});
+
+app.get('/play', (req, res) => {
   fs.readFile(`${__dirname}/../api/index.html`, "utf8", (err, data) => {
     if (err) {
       console.log(err);
