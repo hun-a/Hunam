89afba5aca4c39b9101ca920736f351519b0c86c
seunghun <seunghun_kim@cubrid.com>
2017-08-17 18:12:46 +0900

    add validation script for login

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,25 @@
 $(document).ready(() => {
+  $('#login').click(() => {
+    const id = $('#user').val();
+    const pwd = $('#pwd').val();
+
+    const regex = /[^\w._-]/g;
+    if (regex.test(id)) {
+      alert('Invalid ID');
+      $('$user').focus();
+      return;
+    }
+
+    if (id.length < 4 || pwd.length < 4) {
+      alert('The length of ID or PASSWORD must be bigger than 4.')
+      return;
+    }
+
+    alert(id + pwd);
+
+    $('#login-form').submit();
+  });
+
   $('#join').click(() => {
     window.location.replace('/join');
   });
