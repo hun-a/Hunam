851e24bb09ee605b1afdf7c9fae6123b605b1691
seunghun <seunghun_kim@cubrid.com>
2017-08-17 11:36:20 +0900

    add login router

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 10000;
 const Downloader = require('../utils/Downloader');
 const syncDB = require('../data/sync-db');
@@ -12,9 +13,18 @@ syncDB().then(_ => {
 app.set('view engine', 'pug');
 app.locals.pretty = true;
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  const user = req.body.user;
+  const pwd = req.body.pwd;
+
+  res.send(user + ', ' + pwd);
 });
 
 app.get('/down/:url/:name', (req, res) => {
