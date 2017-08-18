8e61a17138cc59d17f83ae903620d2dbd64210ef
seunghun <seunghun_kim@cubrid.com>
2017-08-18 10:30:17 +0900

    prepare to use the session

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const app = express();
 const user = require('./api/user');
 
@@ -8,6 +9,11 @@ app.locals.pretty = true;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: '!&(^!#!@&*hun!&(^!#!@&*',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/users', user);
 
